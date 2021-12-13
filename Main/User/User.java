853401776ee6988beccec60312a904a4bb9caa77
Martijn <martijnschermers2@gmail.com>
2021-12-13 16:52:29 +0100

    User optimised

--- Main/User/User.java
+++ Main/User/User.java
@@ -2,8 +2,6 @@ package Main.User;
 import java.sql.Date;
 import java.sql.*;
 
-import Main.Database.Database;
-
 public class User {
     private String email; 
     private String name;
@@ -14,10 +12,13 @@ public class User {
     private String country; 
     private String isCourseTaker;
     private String isStaff;
-    private Database db;
+    
+    private String connectionUrl = "jdbc:sqlserver://aei-sql2.avans.nl\\studenten:1443;databaseName=CodeCademy12;user=adidas12;password=MondKap!;";
+    private Connection con = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
 
     public User(String email, String name, Date birthDate, String gender, String address, String residence, String country, String isCourseTaker, String isStaff) {
-        // db.CreateUser(email, name, birthDate, gender, address, residence, country, isStaff, isCourseTaker);
         this.email = email;
         this.name = name;
         this.birthDate = birthDate;
@@ -30,10 +31,6 @@ public class User {
     }
 
     public void CreateUser() {
-        String connectionUrl = "jdbc:sqlserver://aei-sql2.avans.nl\\studenten:1443;databaseName=CodeCademy12;user=adidas12;password=MondKap!;";
-        Connection con = null;
-        Statement stmt = null;
-        ResultSet rs = null;
         try {
             // 'Importeer' de driver die je gedownload hebt.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -52,6 +49,44 @@ public class User {
         catch (Exception e) {
             e.printStackTrace();
         } finally {
+            if (rs != null)
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+            if (stmt != null)
+                try {
+                    stmt.close();
+                } catch (Exception e) {
+                }
+            if (con != null)
+                try {
+                    con.close();
+                } catch (Exception e) {
+                }
+        }
+    }
+
+    public void DeleteUser() {
+        try {
+            // 'Importeer' de driver die je gedownload hebt.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Maak de verbinding met de database.
+            con = DriverManager.getConnection(connectionUrl);
+
+            // Stel een SQL query samen.
+            String SQL = "DELETE FROM User WHERE (" + email + " = "+ email +")";
+            stmt = con.createStatement();
+            // Voer de query uit op de database.
+            rs = stmt.executeQuery(SQL);
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finally {
             if (rs != null)
                 try {
                     rs.close();
