e7b9fc78a03e8be21d0f4b17309022979a008848
lauwIV <64641417+lauwIV@users.noreply.github.com>
2021-12-18 22:47:50 +0100

    sceneUserCreate(): Finished up the GUI and some constraints; Not addign anything to the Server yet

--- Main/GUI/GUI.java
+++ Main/GUI/GUI.java
@@ -1,15 +1,25 @@
 package Main.GUI;
 
+import Main.ContentItem.Course.Course;
+import Main.Database.Database;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 	Stage stage;
 	Scene scene;
+	Database db = new Database();
 
 	@Override
 	public void start(Stage stage) {
@@ -27,15 +37,207 @@ public class GUI extends Application {
 		this.stage.show();
 	}
     
-	// Method for the user to fill in data
-	public void sceneUserForm() {
-		
-		GridPane test = new GridPane();
-		test.getChildren().add(new Label("This is a test"));
-		this.scene = new Scene(test);
-		this.stage.setScene(this.scene);
+	
 
-	}
+
+		// Method for the user to fill in data.
+public void sceneUserCreate() {
+	
+	GridPane gridPane = new GridPane();
+
+
+	// Buttons
+	Button bSubmit = new Button("Submit");
+
+	// Labels;
+	Label lEmail = new Label("Email: ");
+	Label lName = new Label("Name: ");
+	Label lBirthdate = new Label("Birthdate: ");
+	Label lGender = new Label("Gender: ");
+	Label lAddress = new Label("Address:");
+	Label lResidence = new Label("Residence: ");
+	Label lCourse = new Label("Course taker: ");
+	Label lStaff = new Label("Staff: ");
+
+	// Text fields
+	TextField email = new TextField();
+	TextField name = new TextField();
+	DatePicker birthdate = new DatePicker();
+	TextField gender = new TextField();
+	TextField address = new TextField();
+	TextField residence = new TextField();
+	
+	// comboBox
+	ObservableList<String> options = 
+    FXCollections.observableArrayList(
+        "Yes",
+        "No"
+    );
+
+	ComboBox course = new ComboBox<>(options);
+	ComboBox staff = new ComboBox<>(options);
+
+	
+	// Coordinates for the elements
+	gridPane.add(lEmail, 0, 1);
+	gridPane.add(email, 1, 1);
+
+	gridPane.add(lName, 0, 2);
+	gridPane.add(name, 1, 2);
+	
+	gridPane.add(lBirthdate, 0, 3);
+	gridPane.add(birthdate, 1, 3);
+
+	gridPane.add(lGender, 0, 4);
+	gridPane.add(gender, 1, 4);
+
+	gridPane.add(lAddress, 0, 5);
+	gridPane.add(address, 1, 5);
+
+	gridPane.add(lResidence, 0, 6);
+	gridPane.add(residence, 1, 6);
+
+	gridPane.add(lCourse, 0, 7);
+	gridPane.add(course, 1, 7);
+
+	gridPane.add(lStaff, 0, 8);
+	gridPane.add(staff, 1, 8);
+
+	gridPane.add(bSubmit, 1, 9);
+
+	// Styling
+	gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
+	gridPane.setVgap(10); 
+	gridPane.setHgap(10);
+
+	// Functions
+	bSubmit.setOnAction((action) -> {
+
+		// Checks if all the fields are filled in
+		if (email.getText().isEmpty() || name.getText().isEmpty() || gender.getText().isEmpty() || address.getText().isEmpty() || residence.getText().isEmpty()) {
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText("Input not valid");
+			errorAlert.setContentText("Fill in all the fields");
+			errorAlert.showAndWait();
+		}
+
+		// Checks if a date is picked
+		if (birthdate.getValue() == null) {
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText("Input not valid");
+			errorAlert.setContentText("Pick a date");
+			errorAlert.showAndWait();
+		}
+
+		// Checks if they picked either staff or user, cant be staff and user
+		if ((course.getValue() == "Yes" && staff.getValue() == "Yes") || (course.getValue() == "No" && staff.getValue() == "No")) {
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText("Input not valid");
+			errorAlert.setContentText("You have to pick Staff or User");
+			errorAlert.showAndWait();
+		}
+	});
+
+
+	this.scene = new Scene(gridPane, 500, 700);
+	this.stage.setScene(this.scene);
+
+}
+
+
+		// Method to see a overview of the users
+public void sceneUserRead() {
+
+	GridPane gridPane = new GridPane();
+	gridPane.getChildren().add(new Label("Read"));
+
+
+	this.scene = new Scene(gridPane, 500, 500);
+	this.stage.setScene(this.scene);
+
+}
+
+
+		// Method for altering a user
+public void sceneUserUpdate() {
+
+	GridPane gridPane = new GridPane();
+	gridPane.getChildren().add(new Label("Update"));
+
+
+	this.scene = new Scene(gridPane, 500, 500);
+	this.stage.setScene(this.scene);
+
+}
+
+
+		// Method for deleting a user
+public void sceneUserDelete() {		
+
+	GridPane gridPane = new GridPane();
+	gridPane.getChildren().add(new Label("Delete"));
+
+
+	this.scene = new Scene(gridPane, 500, 500);
+	this.stage.setScene(this.scene);
+
+}
+
+
+// Method that gives options for the user to navigate through the CRUD system
+public void sceneUser() {
+
+	// Creating all the buttons	
+	Button bCreate = new Button("Create a user");
+	Button bRead = new Button("View users");
+	Button bUpdate = new Button("Update users");
+	Button bDelete = new Button("Delete users");
+
+	// New Gridpane
+	GridPane gridPane = new GridPane();
+
+	// Specifies coordinates for the buttons
+	gridPane.add(bCreate, 0, 1);
+	gridPane.add(bRead, 1, 1);
+
+	gridPane.add(bUpdate, 0, 2);
+	gridPane.add(bDelete, 1, 2);
+	
+	
+	// Styling
+	gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
+	gridPane.setVgap(10); 
+	gridPane.setHgap(10);
+
+
+	// Button functions
+
+	// bCreate
+	bCreate.setOnAction((action) -> {
+		sceneUserCreate();
+	});
+
+	bRead.setOnAction((action) -> {
+		sceneUserRead();
+	});
+
+	bUpdate.setOnAction((action) -> {
+		sceneUserUpdate();
+	});
+
+	bDelete.setOnAction((action) -> {
+		sceneUserDelete();
+	});
+
+	// Change the scene
+	this.scene = new Scene(gridPane, 500, 500);
+	this.stage.setScene(this.scene);
+
+}
+	
+
+	
+
 
 	// Method sets up the start screen with buttons to navigate
 	public void startScene() {
@@ -53,14 +255,17 @@ public class GUI extends Application {
 		GridPane gridPane = new GridPane();
         
 		// Specifies coordinates for the buttons
-        gridPane.add(bCourse, 0, 1, 1, 1);
-        gridPane.add(bEnroledCourse, 1, 1, 1, 1);
-        gridPane.add(bTop3course, 0, 3, 1, 1);
-        gridPane.add(bWebcast, 0, 2, 1, 1);
-        gridPane.add(bEnroledWebcast, 1, 2, 1, 1);
-        gridPane.add(bTop3webcast, 1, 3, 1, 1);
-        gridPane.add(bCertificates, 0, 4, 1, 1);
-		gridPane.add(bUser, 1, 4, 1, 1);
+        gridPane.add(bCourse, 0, 1);
+        gridPane.add(bEnroledCourse, 1, 1);
+		
+        gridPane.add(bWebcast, 0, 2);
+        gridPane.add(bEnroledWebcast, 1, 2);
+
+		gridPane.add(bTop3course, 0, 3);
+        gridPane.add(bTop3webcast, 1, 3);
+
+        gridPane.add(bCertificates, 0, 4);
+		gridPane.add(bUser, 1, 4);
 		
 		// Styling
 		gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
@@ -75,16 +280,17 @@ public class GUI extends Application {
         bTop3webcast.setMaxWidth(300);
         bCertificates.setMaxWidth(300);
 		bUser.setMaxWidth(300);
-		this.scene = new Scene(gridPane);
+		this.scene = new Scene(gridPane, 500, 500);
 		this.stage.setScene(this.scene);
 
 		// Button functions
 
 		// bUser
 		bUser.setOnAction((event) -> {
-			sceneUserForm();
+			sceneUser();
 		});
 	}
 
+	
 
 }
