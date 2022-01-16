1469d471c704000c94d4f1059907758f8b312e4c
Luca-R04 <Lucarinck04@gmail.com>
2022-01-16 14:02:31 +0100

    Update Certificate.java

--- Main/User/Certificate.java
+++ Main/User/Certificate.java
@@ -1,5 +1,53 @@
 package Main.User;
 
+import Main.Database.Database;
+
 public class Certificate {
-    
+
+    private int certificateID;
+    private int rating;
+    private int staffID;
+    private int courseID;
+    private int CourseTakerID;
+    private Database db = new Database();
+
+    public Certificate(Integer certificateID, Integer rating, Integer staffID, Integer courseID,
+            Integer CourseTakerID) {
+        this.certificateID = certificateID;
+        this.rating = rating;
+        this.staffID = staffID;
+        this.courseID = courseID;
+        this.CourseTakerID = CourseTakerID;
+    }
+
+    public void update(Integer certificateID, Integer rating, Integer staffID, Integer courseID,
+            Integer CourseTakerID) {
+        db.updateCertificate(certificateID, rating, staffID, courseID, CourseTakerID);
+
+        this.rating = rating;
+        this.staffID = staffID;
+        this.courseID = courseID;
+        this.CourseTakerID = CourseTakerID;
+    }
+
+    public int getCertificateID() {
+        return certificateID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public int getCourseTakerID() {
+        return CourseTakerID;
+    }
+
 }
