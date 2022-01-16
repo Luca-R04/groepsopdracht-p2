4be20dae66cc10d8b6c837774a2cd246b1e3bfcd
Luca-R04 <Lucarinck04@gmail.com>
2022-01-16 14:22:14 +0100

    Update CertificateGUI.java

--- Main/GUI/CertificateGUI.java
+++ Main/GUI/CertificateGUI.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class CertificateGUI {
@@ -48,89 +49,155 @@ public class CertificateGUI {
     }
 
     private void sceneCertificateUpdate() {
-
-    }
-
-    private void sceneCertificateRead() {
         GridPane gridPane = new GridPane();
 
-		// Labels;
-		Label lUser = new Label("User: ");
+        // Labels;
+        Label lUser = new Label("User: ");
         Label lCertificates = new Label("Certificate: ");
-		Label lCertificateID = new Label("Certificate ID: ");
         Label lRating = new Label("Rating: ");
-		Label lStaff = new Label("Staff ID: ");
-		Label lCourse = new Label("Course: ");
+        Label lStaff = new Label("Staff ID: ");
+        Label lCourse = new Label("Course: ");
 
         ArrayList<User> users = db.getUsers();
-		ArrayList<String> userEmails = new ArrayList<>();
+        ArrayList<String> userEmails = new ArrayList<>();
 
         for (User user : users) {
             userEmails.add(user.getEmail());
         }
 
-		ObservableList<String> userEmailOptions = FXCollections.observableArrayList(userEmails);
+        ObservableList<String> userEmailOptions = FXCollections.observableArrayList(userEmails);
 
-		// Text fields
-		ComboBox<String> userEmail = new ComboBox<>();
-		userEmail.setItems(userEmailOptions);
+        // Text fields
+        ComboBox<String> userEmail = new ComboBox<>();
+        userEmail.setItems(userEmailOptions);
         ComboBox<String> userCertificates = new ComboBox<>();
-		Label certificateID = new Label();
-		Label rating = new Label();
-        Label staff = new Label();
-		Label course = new Label();
+        TextField certificateID = new TextField();
+        TextField rating = new TextField();
+        TextField staff = new TextField();
+        TextField course = new TextField();
 
-		//When a user is selected from the comboBox the method will display it's certificates
-		userEmail.valueProperty().addListener((obs, oldItem, newItem) -> {
-			if (newItem == null) {
-				certificateID.setText("");
-			} else {
+        // When a user is selected from the comboBox the method will display it's
+        // certificates
+        userEmail.valueProperty().addListener((obs, oldItem, newItem) -> {
+            if (newItem == null) {
+                certificateID.setText("");
+            } else {
                 userCertificates.setItems(db.getCertificates(userEmail.getValue()));
-			}
-		});
+            }
+        });
 
         userCertificates.valueProperty().addListener((obs, oldItem, newItem) -> {
-			if (newItem == null) {
-				certificateID.setText("");
+            if (newItem == null) {
                 rating.setText("");
                 staff.setText("");
                 course.setText("");
-                
-			} else {
+
+            } else {
                 ArrayList<String> certificateDataList = db.getCertificateData(userCertificates.getValue());
 
-                certificateID.setText(certificateDataList.get(0));
                 rating.setText(certificateDataList.get(1));
                 staff.setText(certificateDataList.get(2));
                 course.setText(certificateDataList.get(3));
-			}
-		});
+            }
+        });
 
-		// Coordinates for the elements
-		gridPane.add(lUser, 0, 1);
-		gridPane.add(userEmail, 1, 1);
+        // Coordinates for the elements
+        gridPane.add(lUser, 0, 1);
+        gridPane.add(userEmail, 1, 1);
 
-		gridPane.add(lCertificates, 0, 2);
-		gridPane.add(userCertificates, 1, 2);
+        gridPane.add(lCertificates, 0, 2);
+        gridPane.add(userCertificates, 1, 2);
 
-		gridPane.add(lCertificateID, 0, 3);
-		gridPane.add(certificateID, 1, 3);
+        gridPane.add(lRating, 0, 3);
+        gridPane.add(rating, 1, 3);
 
-		gridPane.add(lRating, 0, 4);
-		gridPane.add(rating, 1, 4);
+        gridPane.add(lStaff, 0, 4);
+        gridPane.add(staff, 1, 4);
 
-		gridPane.add(lStaff, 0, 5);
-		gridPane.add(staff, 1, 5);
+        gridPane.add(lCourse, 0, 5);
+        gridPane.add(course, 1, 5);
+
+        // Styling
+        gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        this.scene = new Scene(gridPane, 700, 700);
+    }
+
+    private void sceneCertificateRead() {
+        GridPane gridPane = new GridPane();
+
+        // Labels;
+        Label lUser = new Label("User: ");
+        Label lCertificates = new Label("Certificate: ");
+        Label lRating = new Label("Rating: ");
+        Label lStaff = new Label("Staff ID: ");
+        Label lCourse = new Label("Course: ");
+
+        ArrayList<User> users = db.getUsers();
+        ArrayList<String> userEmails = new ArrayList<>();
+
+        for (User user : users) {
+            userEmails.add(user.getEmail());
+        }
+
+        ObservableList<String> userEmailOptions = FXCollections.observableArrayList(userEmails);
+
+        // Text fields
+        ComboBox<String> userEmail = new ComboBox<>();
+        userEmail.setItems(userEmailOptions);
+        ComboBox<String> userCertificates = new ComboBox<>();
+        Label rating = new Label();
+        Label staff = new Label();
+        Label course = new Label();
+
+        // When a user is selected from the comboBox the method will display it's
+        // certificates
+        userEmail.valueProperty().addListener((obs, oldItem, newItem) -> {
+            if (newItem == null) {
+            } else {
+                userCertificates.setItems(db.getCertificates(userEmail.getValue()));
+            }
+        });
+
+        userCertificates.valueProperty().addListener((obs, oldItem, newItem) -> {
+            if (newItem == null) {
+                rating.setText("");
+                staff.setText("");
+                course.setText("");
+
+            } else {
+                ArrayList<String> certificateDataList = db.getCertificateData(userCertificates.getValue());
+
+                rating.setText(certificateDataList.get(1));
+                staff.setText(certificateDataList.get(2));
+                course.setText(certificateDataList.get(3));
+            }
+        });
+
+        // Coordinates for the elements
+        gridPane.add(lUser, 0, 1);
+        gridPane.add(userEmail, 1, 1);
+
+        gridPane.add(lCertificates, 0, 2);
+        gridPane.add(userCertificates, 1, 2);
+
+        gridPane.add(lRating, 0, 4);
+        gridPane.add(rating, 1, 4);
+
+        gridPane.add(lStaff, 0, 5);
+        gridPane.add(staff, 1, 5);
 
         gridPane.add(lCourse, 0, 6);
-		gridPane.add(course, 1, 6);
+        gridPane.add(course, 1, 6);
 
-		// Styling
-		gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
-		gridPane.setVgap(10);
-		gridPane.setHgap(10);
+        // Styling
+        gridPane.setStyle("-fx-font-size: 2em; -fx-padding: 2em;");
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
 
-		this.scene = new Scene(gridPane, 700, 700);
+        this.scene = new Scene(gridPane, 700, 700);
     }
 
     public Scene getScene() {
